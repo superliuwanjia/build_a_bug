44c6c3edd91d55acb4a5eff0dfb5a2cab0608536
Raymond Yuan <ray.yuan0@gmail.com>
2017-11-29 10:09:10 -0600

    edr working

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -43,6 +43,7 @@ class DeepQNetwork:
         self.targetModelUpdateFrequency = args.target_model_update_freq
         self.normalizeWeights = args.normalize_weights
         self.preprocess = args.preprocess
+        print(self.preprocess)
 
         self.staleSess = None
 
@@ -68,7 +69,7 @@ class DeepQNetwork:
             self.feat_size = 3 * 2
         elif self.preprocess == 'stack':
             self.feat_size = 3 * 3
-        elif self.preprocess is None:
+        elif self.preprocess == 'None':
             self.feat_size = 4
         else:
             raise NotImplementedError('Preprocess not implemented')
@@ -76,13 +77,13 @@ class DeepQNetwork:
 
         self.x, self.y = self.buildNetwork('policy', True, numActions)
         self.first_time = False
-        if not self.preprocess:
+        if self.preprocess != 'None':
             assert (len(tf.trainable_variables()) == 10),"Expected 10 trainable_variables"
             assert (len(tf.global_variables()) == 10),"Expected 10 total variables"
 
         self.x_target, self.y_target = self.buildNetwork('target', False, numActions)
 
-        if not self.preprocess:
+        if self.preprocess != 'None':
             assert (len(tf.trainable_variables()) == 10),"Expected 10 trainable_variables"
             assert (len(tf.global_variables()) == 20),"Expected 20 total variables"
 
@@ -152,19 +153,26 @@ class DeepQNetwork:
 
             event = self.tf_retina(t_frames)
             if self.preprocess == 'stack':
+                t_frames = tf.to_float(t_frames)
                 t_frames = tf.expand_dims(t_frames, len(frames_shape))
-                event = tf.concat((event, t_frames), len(frames_shape))
-            event_shape = event.get_shape().as_list()
+                event = tf.concat([event, t_frames], len(frames_shape))
+            # event_shape = event.get_shape().as_list()
+            event_shape = tf.shape(event)
+            print('event shape', event_shape)
             # print(np.amax(event), np.amin(event), np.mean(event), event.shape)
+            # 4, 32, 84, 84, (2, 3)
             event = tf.transpose(event, (1, 2, 3, 4, 0))
+            # 32, 84, 84, (2, 3), 4
             event = event[:, :, :, :, 1:]
+            self.event = event
             # print
             # for i in range(4):
             #     print('two', np.amax(event[0,:,:,1,i]), np.amin(event[0,:,:,1,i]), np.mean(event[0,:,:,1,i]), event[0,:,:,1,i].shape)
             event_out = tf.reshape(event, (event_shape[1], event_shape[2], event_shape[3], event_shape[4] * (event_shape[0] - 1)))
-            
-        elif self.preprocess is None:
+
+        elif self.preprocess == 'None':
             event_out = tf.to_float(input_frames) / 255.0
+            self.event = event_out
         else:
             raise NotImplementedError('Preprocess type not implemented')
 
@@ -206,19 +214,17 @@ class DeepQNetwork:
         """
         # decay constant must be smaller than 1
         # assert alpha <= 1 and alpha >= 0, 'decay constant must be positive and less than 1'
-
-        ema = np.zeros(video.shape, dtype=float)
-
-        # normalize video pixel value to 0 ~ 1
+        video_shape = video.get_shape().as_list()
         video = tf.to_float(video) + 1e-3
-
-        # EMA filtering
-        ema[0, :, :, :] = video[0, :, :, :]
-
-        for t in range(1, video.shape[0]):
-            ema[t, :, :, :] = (1 - alpha) * ema[t - 1, :, :, :] + \
-                              alpha * video[t, :, :, :]
-
+        print(video.get_shape().as_list())
+        assert video_shape[0] == 4, 'time not first dim'
+        split_vids = tf.split(video, num_or_size_splits=video_shape[0], axis=0)
+        emas = [split_vids[0]]
+        for t in range(1, video_shape[0]):
+            emas.append((1 - alpha) * emas[t - 1] + alpha * split_vids[t])
+        print([temp.get_shape().as_list() for temp in emas])
+        ema = tf.concat(emas, axis=0)
+        print(ema.get_shape().as_list())
         # compute relative change
         change = tf.tanh(tf.log(tf.divide(video, ema + 1e-5)))
 
@@ -234,7 +240,7 @@ class DeepQNetwork:
             frames_shape = input_frames.shape
             t_frames = np.transpose(input_frames, (3, 0, 1, 2))
 
-            event = self.retina(t_frames)
+            event = self.np_retina(t_frames)
             if self.preprocess == 'stack':
                 t_frames = np.expand_dims(t_frames, len(frames_shape))
                 event = np.concatenate((event, t_frames), len(frames_shape))
@@ -247,7 +253,7 @@ class DeepQNetwork:
             #     print('two', np.amax(event[0,:,:,1,i]), np.amin(event[0,:,:,1,i]), np.mean(event[0,:,:,1,i]), event[0,:,:,1,i].shape)
             event_out = np.reshape(event, (event_shape[1], event_shape[2], event_shape[3], event_shape[4] * (event_shape[0] - 1)))
             # print('three', np.amax(event_out[0,:,:,0]), np.amin(event_out[0,:,:,0]), np.mean(event_out[0,:,:,0]), event_out[0,:,:,0].shape)
-        elif self.preprocess is None:
+        elif self.preprocess == 'None':
             event_out = input_frames / 255.0
         else:
             raise NotImplementedError('Preprocess type not implemented')
@@ -258,19 +264,21 @@ class DeepQNetwork:
         self.trainable = trainable
         print("Building network for %s trainable=%s" % (name, trainable))
         # First layer takes a screen, and shrinks by 2x
-        x = tf.placeholder(tf.float32, shape=[None, 84, 84, self.feat_size], name="preproc_screens")
+        # x = tf.placeholder(tf.float32, shape=[None, 84, 84, self.feat_size], name="preproc_screens")
+        x = self.input
         # None, 84, 84, 4
         # print("Input shape to preproc {}".format(inp_shape))
         # dqn_in = self.applyPreprocess(x, inp_shape)
-        # dqn_in_shape = dqn_in.get_shape().as_list()
+        dqn_in = self.tf_apply_preprocess(x)
+        dqn_in_shape = dqn_in.get_shape().as_list()
         # self.feat_size = dqn_in_shape[-1]
-        # if self.args.testing: print("After preprocessing input shape {}".format(dqn_in_shape))
+        if self.args.testing: print("After preprocessing input shape {}".format(dqn_in_shape))
 
         # Second layer convolves 32 8x8 filters with stride 4 with relu
         with tf.variable_scope("cnn1_" + name):
             W_conv1, b_conv1 = self.makeLayerVariables([8, 8, self.feat_size, 32], trainable, "conv1")
 
-            h_conv1 = tf.nn.relu(tf.nn.conv2d(x, W_conv1, strides=[1, 4, 4, 1], padding='VALID') + b_conv1, name="h_conv1")
+            h_conv1 = tf.nn.relu(tf.nn.conv2d(dqn_in, W_conv1, strides=[1, 4, 4, 1], padding='VALID') + b_conv1, name="h_conv1")
             print(h_conv1)
 
         # Third layer convolves 64 4x4 filters with stride 2 with relu
@@ -319,8 +327,8 @@ class DeepQNetwork:
         return weights, biases
 
     def inference(self, screens):
-        proc_screens = self.apply_preprocess(screens)
-        y = self.sess.run([self.y], {self.x: proc_screens})
+        # proc_screens = self.apply_preprocess(screens)
+        y = self.sess.run([self.y], {self.x: screens})
         q_values = np.squeeze(y)
         return np.argmax(q_values)
 
@@ -343,27 +351,7 @@ class DeepQNetwork:
         proc_x = self.apply_preprocess(x)
         # print('three', np.amax(proc_x), np.amin(proc_x), np.mean(proc_x), proc_x.shape)
         '''
-
-        if self.args.save_imgs and stepNumber % 100 == 0:
-            print('Saving debug images to debugImages')
-            dir_save_main = self.baseDir + '/debugImages'
-            if not os.path.isdir(dir_save_main):
-                os.makedirs(dir_save_main)
-
-            dir_save = dir_save_main + '/{}/'.format(stepNumber)
-            if not os.path.isdir(dir_save):
-                os.makedirs(dir_save)
-            scipy.misc.imsave(dir_save + 'normFrame{}.png'.format(stepNumber), x[0, :, :, 0])
-            scipy.misc.imsave(dir_save + 'retinaFramesOn{}.png'.format(stepNumber), proc_x[0, :, :, 0])
-            scipy.misc.imsave(dir_save + 'retinaFramesOff{}.png'.format(stepNumber), proc_x[0, :, :, 1])
-            r_off = proc_x[0, :, :, 1]
-            r_on = proc_x[0, :, :, 0]
-            print("TEST TEST", np.amin(r_off[np.nonzero(r_off)]))
-            assert np.amin(r_off) >= 0 and np.amin(r_on) >= 0, 'retina output < 0'
-            print("RETINA ON -  MEAN: {}, MAX: {}, MIN: {}".format(np.mean(r_on), np.amax(r_on), np.amin(r_on)))
-            print("RETINA OFF-  MEAN: {}, MAX: {}, MIN: {}".format(np.mean(r_off), np.amax(r_off), np.amin(r_off)))
-
-        y2 = self.y_target.eval(feed_dict={self.x_target: proc_x2}, session=self.sess)
+        y2 = self.y_target.eval(feed_dict={self.x_target: x2}, session=self.sess)
         a = np.zeros((len(batch), self.numActions))
         y_ = np.zeros(len(batch))
 
@@ -374,13 +362,34 @@ class DeepQNetwork:
             else:
                 y_[i] = batch[i].reward + gamma * np.max(y2[i])
 
-        _, sum_str = self.sess.run([self.train_step, self.merged], feed_dict={
-                                                                            self.x: proc_x,
+        _, sum_str, proc_x = self.sess.run([self.train_step, self.merged, self.event], feed_dict={
+                                                                            self.x: x,
                                                                             self.a: a,
                                                                             self.y_: y_
                                                                         })
         self.summary_writer.add_summary(sum_str, stepNumber)
         self.summary_writer.flush()
+
+        if self.args.save_imgs and stepNumber % 100 == 0:
+            print('Saving debug images to debugImages')
+            dir_save_main = self.baseDir + '/debugImages'
+            if not os.path.isdir(dir_save_main):
+                os.makedirs(dir_save_main)
+
+            dir_save = dir_save_main + '/{}/'.format(stepNumber)
+            if not os.path.isdir(dir_save):
+                os.makedirs(dir_save)
+            scipy.misc.imsave(dir_save + 'normFrame{}.png'.format(stepNumber), np.array(x)[0, :, :, 0])
+            scipy.misc.imsave(dir_save + 'retinaFramesOn{}.png'.format(stepNumber), proc_x[0, :, :, 0])
+            scipy.misc.imsave(dir_save + 'retinaFramesOff{}.png'.format(stepNumber), proc_x[0, :, :, 1])
+            if self.preprocess == 'stack':
+                scipy.misc.imsave(dir_save + 'Orig frames stacked{}.png'.format(stepNumber), proc_x[0, :, :, 2])
+            r_off = proc_x[0, :, :, 1]
+            r_on = proc_x[0, :, :, 0]
+            print("TEST TEST", np.amin(r_off[np.nonzero(r_off)]))
+            assert np.amin(r_off) >= 0 and np.amin(r_on) >= 0, 'retina output < 0'
+            print("RETINA ON -  MEAN: {}, MAX: {}, MIN: {}".format(np.mean(r_on), np.amax(r_on), np.amin(r_on)))
+            print("RETINA OFF-  MEAN: {}, MAX: {}, MIN: {}".format(np.mean(r_off), np.amax(r_off), np.amin(r_off)))
 
         if stepNumber % self.targetModelUpdateFrequency == 0:
 			self.sess.run(self.update_target)
